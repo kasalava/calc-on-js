ff284f36664ae899a7390e47da1d1e9ae17aa72f
kasalava <kasalava86@gmail.com>
2025-06-04 00:58:20 +0500

    end

--- calc.js
+++ calc.js
@@ -1,5 +1,25 @@
-function +(){
-    var num1, num2
-    num1 = document.getElementById('n1').value//получаем элемент по его идентификатору. Value - это св-во которое отвечает, за то что пользователь ввел дынные
-    num1 = parseInt(num1)//ф-я котора проверит содержимое инпута и преоюразует его в целое число (5а->5; 5.1->5)
+function plus() {
+    var num1, num2, result;
+    num1 = document.getElementById('n1').value;//получаем элемент по его идентификатору. Value - это св-во которое отвечает, за то что пользователь ввел дынные
+    num1 = parseInt(num1);//ф-я котора проверит содержимое инпута и преоюразует его в целое число (5а->5; 5.1->5)
+    
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    
+    result=num1 + num2;
+    
+//innerHTML - св-во для записи внутрь тэга html
+    document.getElementById('out').innerHTML=result;
+}
+
+function minus() {
+    var num1, num2, result;
+    num1 = document.getElementById('n1').value;//получаем элемент по его идентификатору. Value - это св-во которое отвечает, за то что пользователь ввел дынные
+    num1 = parseInt(num1);//ф-я котора проверит содержимое инпута и преоюразует его в целое число (5а->5; 5.1->5)
+    
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    
+    result=num1 - num2;
+      document.getElementById('out').innerHTML=result;
 }
